67ac0beb06d8bab3421ed7a32aa8b85135f8c6d0
Koushik Kothagal <koush.s@gmail.com>
2023-02-17 20:54:11 -0800

    Session 3 exercises

---
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/EqualsAndHashCode.java
@@ -0,0 +1,14 @@
+package io.javabrains.javacollections;
+
+/*
+Generate a Person class with the member variables firstName, lastName, age, lastModifiedDate.
+Generate equals and hashcode methods using member variables firstName, lastName, age
+Compare two instances of Person that have the same data
+*/
+
+public class EqualsAndHashCode {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListExercise.java
@@ -0,0 +1,13 @@
+package io.javabrains.javacollections;
+
+/*
+Initialize an ArrayList, set the value to a few integers
+Then write code to find the sum of all integers
+ */
+
+public class ListExercise {
+
+    public static void main(String[] args) {
+
+    }
+}

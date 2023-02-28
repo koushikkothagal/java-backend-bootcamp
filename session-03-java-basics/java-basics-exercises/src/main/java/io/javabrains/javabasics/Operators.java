c475030998ba6fcf1b9dbe4979ce079b76368e0f
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-02-28 22:08:29 +0530

    c

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
@@ -6,31 +6,34 @@ import java.util.List;
 public class Operators {
     public static void main(String[] args) {
         // Step 1: Declare variables
-        ArrayList
+        int a=5,b=5;
 
         // Step 2: Perform arithmetic operations
-
+       int sum=a+b;
+       
 
         // Step 3: Print the results of the arithmetic operations
-
+        System.out.println("Sum = "+sum);
 
         // Step 4: Perform increment and decrement operations
-
+a++;
+b++;
 
         // Step 5: Print the results of the increment and decrement operations
-
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
 
         // Step 6: Perform comparison operations
-
+boolean isBGreater=b>a;
 
         // Step 7: Print the results of the comparison operations
 
-
+System.out.println("IsBGreater = "+isBGreater);
         // Step 8: Perform logical operations
-
-
+        boolean isAGreater=a>b;
+        boolean isAlwaysFalse = !isAGreater && !isBGreater;
         // Step 9: Print the results of the logical operations
-
+        System.out.println("isAlwaysFalse = "+isAlwaysFalse);
 
     }
 }

4a83e3a56f2f347db8d232e87f742a328f815252
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-03 21:57:58 +0530

    c

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InheritanceChallenge.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InheritanceChallenge.java
@@ -11,15 +11,83 @@ package io.javabrains.javabasics;
 7.  Loop through the array of `Animal` objects and print the name, age, and sound of each animal.
  */
 
-
-public class InheritanceChallenge {
-
+ class Animal{
+  private String name;
+  private int age;
+  private String breed;
+  public Animal(String name,int age,String breed){
+    this.name=name;
+    this.age=age;
+    this.breed=breed;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getAge(){
+    return age;
+  }
+  public void makeSound(){
+    System.out.println("Animals make sound");
+  }
+  public String getBreed(){
+    return breed;
+  }
+}
+class Dog extends Animal{
+  public Dog(String name,int age,String breed){
+     super(name, age ,breed);
+  }
+  public void makeSound(){
+    System.out.println("Dog barks");
+  }
+}
+class Cat extends Animal{
+  private int lives;
+  public Cat(String name,int age,int lives,String breed){
+          super(name, age ,breed);
+     this.lives=lives;
+  }
+  public void makeSound(){
+    System.out.println("Cat meows");
+  }
+  public void getLives(){
+    System.out.println("Cats have "+lives+" lives");
+  }
+}
+class Lion extends Animal{
+    
+  public Lion(String name,int age, String breed){
+     super(name, age ,breed);
+  }
+  public void makeSound(){
+    System.out.println("Lion roars");
+  }
+}
+class Cow extends Animal{
+    
+  public Cow(String name,int age, String breed){
+      super(name, age ,breed);
+  }
+  public void makeSound(){
+    System.out.println("Cow moos");
+  }
+}
+public class Main {
     public static void main(String[] args) {
       Animal[] animals=new Animal[4];
-      animals[0]=new Lion('Simba',2);
-      animals[1]=new Dog('Puppy',3);
-      animals[0]=new Cat('Kitty',2);
-      animals[0]=new Cow('Pearl',2);
-
+      animals[0]=new Lion("Simba",2,"AfricanLion");
+      animals[1]=new Dog("Puppy",3,"IndianPeriah");
+      animals[2]=new Cat("Kitty",2,9,"Bengalcat");
+      animals[3]=new Cow("Pearl",2, "Bargur");
+      for(Animal animal:animals){
+        System.out.println("Name: "+animal.getName());
+        System.out.println("Age: "+animal.getAge());
+        System.out.println("Breed: "+animal.getBreed());
+        if(animal instanceof Cat) {
+            ((Cat)animal).getLives();
+        }
+        animal.makeSound();
+        System.out.println();
+      }
     }
 }

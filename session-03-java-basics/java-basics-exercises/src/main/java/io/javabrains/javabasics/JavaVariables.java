a76b4b73ab0c719341a553b7e0453b992367015d
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-03 22:59:00 +0530

    c

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/JavaVariables.java
@@ -10,23 +10,16 @@ public class JavaVariables {
          long l=10;
         // float variable to store a floating-point value
         float f=2.5;
-
         // double variable to store a double value
         double d=3.14;
-
         // char variable to store a single character
          char c='A';
-
         // boolean variable to store a boolean value
         boolean b=true;
-
         // String variable to store a string of characters
         string s="Hi";
-
         // Step 2: Print the values of the variables to the console
         System.out.println("a = " + a + "\n" + "l = " + l + "\n" + "f = " + f + "\n" + "d = " + d + "\n" + "c = " + c + "\n" + "b = " + b + "\n" + "s = " + s);
-
-
         // Step 3: Modify the values of the variables and print the updated values
         a=20;
         l=20000;
@@ -36,6 +29,5 @@ public class JavaVariables {
         b=false;
         s="hello";
         System.out.println("a = " + a + "\n" + "l = " + l + "\n" + "f = " + f + "\n" + "d = " + d + "\n" + "c = " + c + "\n" + "b = " + b + "\n" + "s = " + s);
-
     }
 }

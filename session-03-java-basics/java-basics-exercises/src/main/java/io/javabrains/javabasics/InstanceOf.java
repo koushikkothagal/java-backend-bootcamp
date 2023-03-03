fee2d1b286b7fe22baed17d80599fda0db0cb741
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-03 22:59:39 +0530

    c

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/InstanceOf.java
@@ -1,16 +1,13 @@
 package io.javabrains.javabasics;
 
 public class InstanceOf {
-
     public static void main(String[] args) {
-
         Object[] objects = {
                 10,
                 "Hello",
                 true,
                 20.5
         };
-
         // Challenge: Use instanceof to determine the type of each object in the array
        for(Object a:objects){
         if(a instanceof Integer){

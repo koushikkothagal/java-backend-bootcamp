a776ba241a8d092c964bbcd57cf062e574417bfe
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-04 11:11:49 +0530

    c

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/IteratorExercise.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/IteratorExercise.java
@@ -2,14 +2,22 @@ package io.javabrains.javacollections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /*
 Write code that calls the getFruits method and then iterates over the collection, printing each element to console
  */
 
 public class IteratorExercise {
-
-
+ public static void main(STring[] args){
+    FruitSupplier fruitSupplier=new FruitSupplier();
+    Collection<String> fruits=fruitSupplier.getFruits();
+    Iterator<String> iterator=fruits.iterator();
+    while(iterator.hasNext()){
+        String fruitstr=iterator.next();
+        System.out.println("fruitstr = "+fruitstr);
+    }
+ }
 }
 
 

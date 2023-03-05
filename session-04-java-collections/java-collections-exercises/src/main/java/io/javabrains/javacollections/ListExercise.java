6551a17d6b92f58c04149f4fc9b6596e9d85a4f4
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-05 14:53:59 +0530

    commit

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListExercise.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListExercise.java
@@ -1,13 +1,13 @@
 package io.javabrains.javacollections;
-
+import java.util.*;
 /*
 Initialize an ArrayList, set the value to a few integers
 Then write code to find the sum of all integers
  */
-
 public class ListExercise {
-
     public static void main(String[] args) {
-
+ ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        int sum = numbers.stream().mapToInt(Integer::intValue).sum();
+        System.out.println("The sum of the integers in the list is " + sum);
     }
 }

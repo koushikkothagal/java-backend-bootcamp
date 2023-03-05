ab5d8b869a3c906778ed6e02d23f8da218fbcee3
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2023-03-05 11:41:17 +0530

    c

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListIteratorExercise.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListIteratorExercise.java
@@ -1,14 +1,10 @@
 package io.javabrains.javacollections;
-
 /*
 Print the list in forward and reverse order using iterators only
 */
-
 import java.util.ArrayList;
 import java.util.ListIterator;
-
 public class ListIteratorExercise {
-
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>();
         list.add("Item 1");
@@ -16,6 +12,17 @@ public class ListIteratorExercise {
         list.add("Item 3");
         list.add("Item 4");
         list.add("Item 5");
-
+        System.out.println("Forward order of the list:");
+        ListIterator<String>forward=list.listIterator();
+        while(forward.hasNext()){
+            System.out.print(forward.next()+" ");
+        }
+        System.out.println();
+        System.out.println("Reverse order of the list:");
+        ListIterator<String>reverse=list.listIterator(list.size());
+        while(reverse.hasPrevious()){
+            System.out.print(reverse.previous()+" ");
+        }
+        System.out.println();
     }
 }

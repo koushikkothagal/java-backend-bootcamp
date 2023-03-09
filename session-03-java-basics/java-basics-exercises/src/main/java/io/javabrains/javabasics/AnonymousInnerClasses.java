dfdf0cb0288095811e1b636ef17e491e45b7a3c6
Loganayaki2003 <107347212+Loganayaki2003@users.noreply.github.com>
2023-03-09 20:44:11 +0530

    Update AnonymousInnerClasses.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/AnonymousInnerClasses.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/AnonymousInnerClasses.java
@@ -4,9 +4,14 @@ package io.javabrains.javabasics;
 Create a class `Animal2` with an abstract method `makeSound`.
 Create an anonymous inner class implementation of `Animal2` and then call the `makeSound` method
  */
-
+class Animal2{
+    public void makeSound(){
+        System.out.println("MEOW");
+    }
+}
 public class AnonymousInnerClasses {
     public static void main(String[] args) {
-
+       Animal2 obj=new Animal2();
+        obj.makeSound();
     }
 }

51cd72ca34db5b8c31f56f17e3097a2d9c6dc718
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2023-03-13 12:16:47 +0530

    Update Operators.java

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
@@ -1,36 +1,40 @@
 package io.javabrains.javabasics;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Operators {
     public static void main(String[] args) {
         // Step 1: Declare variables
-        ArrayList
+        int num1=15;
+        int num2=10;
 
         // Step 2: Perform arithmetic operations
-
+        int sum=num1+num2;
+        int diff=num1-num2;
 
         // Step 3: Print the results of the arithmetic operations
-
+        System.out.println("sum = " + sum);
+        System.out.println("diff = " + diff);
 
         // Step 4: Perform increment and decrement operations
-
+        num1++;
+        num2++;
 
         // Step 5: Print the results of the increment and decrement operations
-
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
 
         // Step 6: Perform comparison operations
-
+        boolean comp= num1>num2;
+        boolean equals= num1 == num2;
 
         // Step 7: Print the results of the comparison operations
-
+        System.out.println("comp = " + comp);
+        System.out.println("equals = " + equals);
 
         // Step 8: Perform logical operations
-
+        boolean logicalOperation= num1>num2 && num1== num2;
 
         // Step 9: Print the results of the logical operations
-
+        System.out.println("logicalOperation = " + logicalOperation);
 
     }
 }

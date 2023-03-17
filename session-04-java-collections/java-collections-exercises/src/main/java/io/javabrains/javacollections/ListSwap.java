ae914030ab895bf1d19a6953c12e9464aa494434
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2023-03-17 16:56:46 +0530

    Update ListSwap.java

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListSwap.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListSwap.java
@@ -5,9 +5,28 @@ Write a method that accepts a list and two position ints and swaps the elements 
 The method should be generic and accept any List
 */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListSwap {
 
     public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println("Before Swapping" +list);
+
+        System.out.println("After swapping" +swap(list,1,3));
+    }
+
+    private static List swap(List<Object> list, int index1, int index2) {
+        Object e = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, e);
+        return list;
 
     }
 }

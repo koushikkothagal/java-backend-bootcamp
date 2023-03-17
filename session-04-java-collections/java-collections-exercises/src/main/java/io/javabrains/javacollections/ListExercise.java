7ba07d7c0ead2e2a222ed7953fc4cbd22b8bd6b5
SaiPavanN01 <88645382+SaiPavanN01@users.noreply.github.com>
2023-03-17 16:55:03 +0530

    Update ListExercise.java

--- session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListExercise.java
+++ session-04-java-collections/java-collections-exercises/src/main/java/io/javabrains/javacollections/ListExercise.java
@@ -5,10 +5,26 @@ Initialize an ArrayList, set the value to a few integers
 Then write code to find the sum of all integers
  */
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ListExercise {
 
     public static void main(String[] args) {
-
+        List<Integer> list = new ArrayList<>();
+        int sum=0;
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        Iterator<Integer> itr = list.iterator();
+        while(itr.hasNext()){
+            sum+= itr.next();
+        }
+        System.out.println("sum = " + sum);
     }
 
 }
+

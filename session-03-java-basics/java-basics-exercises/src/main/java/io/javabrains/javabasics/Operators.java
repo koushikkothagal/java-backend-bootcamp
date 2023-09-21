560593ed1bb39e16cfaa9f3383f2ff8a287bec4f
Kushal Basu <kushalbasu2@gmail.com>
2023-09-22 00:08:05 +0530

    Excercise 3

--- session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
+++ session-03-java-basics/java-basics-exercises/src/main/java/io/javabrains/javabasics/Operators.java
@@ -1,36 +1,34 @@
 package io.javabrains.javabasics;
 
+import javax.lang.model.SourceVersion;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Operators {
     public static void main(String[] args) {
         // Step 1: Declare variables
-        //ArrayList
+        int a = 10;
+        int b = 20;
 
         // Step 2: Perform arithmetic operations
-
-
+        int sum = a+b;
         // Step 3: Print the results of the arithmetic operations
-
+        System.out.println("sum = " + sum);
 
         // Step 4: Perform increment and decrement operations
-
+        a++;
+        b++;
 
         // Step 5: Print the results of the increment and decrement operations
-
-
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
         // Step 6: Perform comparison operations
-
-
+        boolean bool = a==b;
         // Step 7: Print the results of the comparison operations
-
-
+        System.out.println("bool = " + bool);
         // Step 8: Perform logical operations
-
-
+        Boolean bool2 = sum > a && sum > b;
         // Step 9: Print the results of the logical operations
-
-
+        System.out.println("bool2 = " + bool2);
     }
 }
